bd6ee72b4d8a906fd67aa0605081907790a8528a
Marcos Eduardo <marcos.edaurdo22@gmail.com>
2023-05-07 19:14:50 -0300

    adionando o numero de clientes na fila

--- main.py
+++ main.py
@@ -2,18 +2,23 @@ import random                           # gerador de números aleatórios
 import simpy                            # biblioteca de simulação
 
 # tempo médio entre chegadas sucessivas de clientes
-TEMPO_MEDIO_CHEGADAS = 1.0
+TEMPO_MEDIO_CHEGADAS = 0.5
 TEMPO_MEDIO_ATENDIMENTO = 0.5           # tempo médio de atendimento no servidor
+# variável global para controlar o número de clientes na fila
+fila = 0
 
 
 def geraChegadas(env):
+    global fila
     # função que cria chegadas de entidades no sistema
     contaChegada = 0
     while True:
         # aguardo um intervalo de tempo exponencialmente distribuído
         yield env.timeout(random.expovariate(1.0/TEMPO_MEDIO_CHEGADAS))
         contaChegada += 1
-        print('%.1f Chegada do cliente %d' % (env.now, contaChegada))
+        fila += 1                         # um novo cliente chegou, incrementa a variável fila
+        print('%.1f Chegada do cliente %d. %d cliente(s) na fila.' %
+              (env.now, contaChegada, fila))
 
         # inicia o processo de atendimento
         env.process(atendimentoServidor(env, "cliente %d" %
@@ -21,6 +26,7 @@ def geraChegadas(env):
 
 
 def atendimentoServidor(env, nome, servidorRes):
+    global fila
     # função que ocupa o servidor e realiza o atendimento
     # solicita o recurso servidorRes
     request = servidorRes.request()
@@ -32,7 +38,8 @@ def atendimentoServidor(env, nome, servidorRes):
     # aguarda um tempo de atendimento exponencialmente distribuído
     yield env.timeout(random.expovariate(1.0/TEMPO_MEDIO_ATENDIMENTO))
     print('%.1f Servidor termina o atendimento do %s' % (env.now, nome))
-
+    # o cliente foi atendido e saiu da fila, decrementa a variável fila
+    fila -= 1
     # libera o recurso servidorRes
     yield servidorRes.release(request)
 
@@ -44,4 +51,4 @@ servidorRes = simpy.Resource(env, capacity=1)   # cria o recurso servidorRes
 # incia processo de geração de chegadas
 env.process(geraChegadas(env))
 
-env.run(until=20)                                # executa o modelo por 5 min
+env.run(until=10)                                # executa o modelo por 5 min
