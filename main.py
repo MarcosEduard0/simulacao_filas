d908e9e293c1cda485efb3820200475f18623965
Marcos Eduardo <marcos.edaurdo22@gmail.com>
2023-05-08 01:34:07 -0300

    contagem de clientes

--- main.py
+++ main.py
@@ -4,21 +4,17 @@ import simpy                            # biblioteca de simulação
 # tempo médio entre chegadas sucessivas de clientes
 TEMPO_MEDIO_CHEGADAS = 0.5
 TEMPO_MEDIO_ATENDIMENTO = 0.5           # tempo médio de atendimento no servidor
-# variável global para controlar o número de clientes na fila
-fila = 0
 
 
 def geraChegadas(env):
-    global fila
     # função que cria chegadas de entidades no sistema
     contaChegada = 0
     while True:
         # aguardo um intervalo de tempo exponencialmente distribuído
         yield env.timeout(random.expovariate(1.0/TEMPO_MEDIO_CHEGADAS))
         contaChegada += 1
-        fila += 1                         # um novo cliente chegou, incrementa a variável fila
-        print('%.1f Chegada do cliente %d. %d cliente(s) na fila.' %
-              (env.now, contaChegada, fila))
+        print('%.1f Chegada do cliente %d. Cliente(s) na fila: %d.' %
+              (env.now, contaChegada, len(servidorRes.queue)))
 
         # inicia o processo de atendimento
         env.process(atendimentoServidor(env, "cliente %d" %
@@ -26,7 +22,6 @@ def geraChegadas(env):
 
 
 def atendimentoServidor(env, nome, servidorRes):
-    global fila
     # função que ocupa o servidor e realiza o atendimento
     # solicita o recurso servidorRes
     request = servidorRes.request()
@@ -39,7 +34,6 @@ def atendimentoServidor(env, nome, servidorRes):
     yield env.timeout(random.expovariate(1.0/TEMPO_MEDIO_ATENDIMENTO))
     print('%.1f Servidor termina o atendimento do %s' % (env.now, nome))
     # o cliente foi atendido e saiu da fila, decrementa a variável fila
-    fila -= 1
     # libera o recurso servidorRes
     yield servidorRes.release(request)
 
