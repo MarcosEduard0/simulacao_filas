47e4c3cdd1de06c5fa20db3af0dc40d0f94ff04c
Marcos Eduardo <marcos.edaurdo22@gmail.com>
2023-05-30 01:31:50 -0300

    adding

--- interface web/simulador.py
+++ interface web/simulador.py
@@ -4,6 +4,7 @@ import numpy as np
 def simulador_1(lambda_=1, mi_=2, simulation_total_time=10000, deterministic=False):
     n = simul_time = number_of_arrivals = system_spent_time = clients_in_system = clients_in_line = last_event_time = 0
     line_queue, arrivals, wait_times = [], [], []
+    number_of_departure = 0
     exponential = (
         lambda mi: 1/mi) if deterministic else (lambda mi: np.random.exponential(scale=1/mi))
 
@@ -24,6 +25,7 @@ def simulador_1(lambda_=1, mi_=2, simulation_total_time=10000, deterministic=Fal
                 line_queue.append(simul_time)
 
         else:
+            number_of_departure += 1
             simul_time += departure_time
             clients_in_system += (simul_time - last_event_time) * n
             clients_in_line += (simul_time - last_event_time) * len(line_queue)
@@ -38,6 +40,8 @@ def simulador_1(lambda_=1, mi_=2, simulation_total_time=10000, deterministic=Fal
     avg_system_time = system_spent_time / number_of_arrivals
     avg_clients_in_system = clients_in_system / simul_time
     avg_clients_in_line = clients_in_line / simul_time
+    avg_number_of_departure = number_of_departure/simul_time
+    avg_number_of_arrivals = number_of_arrivals/simul_time
     decimal = 2
 
     results = {
@@ -49,6 +53,9 @@ def simulador_1(lambda_=1, mi_=2, simulation_total_time=10000, deterministic=Fal
         'simul_time': round(simul_time, decimal),
         'number_of_arrivals': round(number_of_arrivals, decimal),
         'wait_times': wait_times,
+        'avg_number_of_arrivals': avg_number_of_arrivals,
+        'avg_number_of_departure': avg_number_of_departure,
+
     }
 
     return results
@@ -57,6 +64,7 @@ def simulador_1(lambda_=1, mi_=2, simulation_total_time=10000, deterministic=Fal
 def simulador_2(lambda_=1, mi_=2, simulation_total_time=10000, deterministic=False):
     n, number_of_arrivals, waiting_line_time, system_spent_time, clients_in_system, clients_in_line, last_event_time = [
         0]*7
+    number_of_departure = 0
     queue, line_queue, arrivals, queue_evolution, event = [], [], [], [], [0, ""]
     queue.append([np.random.exponential(scale=1/lambda_), "Chegada"])
 
@@ -81,6 +89,7 @@ def simulador_2(lambda_=1, mi_=2, simulation_total_time=10000, deterministic=Fal
                 line_queue.append(event[0])
 
         else:
+            number_of_departure += 1
             if len(line_queue) > 0:
                 waiting_line_time += event[0] - line_queue.pop(0)
             n -= 1
@@ -96,6 +105,8 @@ def simulador_2(lambda_=1, mi_=2, simulation_total_time=10000, deterministic=Fal
     avg_system_time = system_spent_time / number_of_arrivals
     avg_clients_in_system = clients_in_system / event[0]
     avg_clients_in_line = clients_in_line / event[0]
+    avg_number_of_departure = number_of_departure/event[0]
+    avg_number_of_arrivals = number_of_arrivals/event[0]
 
     decimal = 2
     results = {
@@ -106,7 +117,9 @@ def simulador_2(lambda_=1, mi_=2, simulation_total_time=10000, deterministic=Fal
         'people_in_line': n,
         'simul_time': event[0],
         'number_of_arrivals': round(number_of_arrivals, decimal),
-        'queue_evolution': queue_evolution
+        'queue_evolution': queue_evolution,
+        'avg_number_of_arrivals': avg_number_of_arrivals,
+        'avg_number_of_departure': avg_number_of_departure
     }
 
     return results
@@ -115,7 +128,7 @@ def simulador_2(lambda_=1, mi_=2, simulation_total_time=10000, deterministic=Fal
 def rodadas_simulacao(lambda_=1, mi_=2, simulation_total_time=10000, sample_size=1, deterministic=False, simulador=None):
     results = {}
     keys = ['avg_clients_in_system', 'avg_clients_in_line',
-            'avg_system_time', 'avg_waiting_time']
+            'avg_system_time', 'avg_waiting_time', 'avg_number_of_departure']
     for key in keys:
         results[key] = []
 
